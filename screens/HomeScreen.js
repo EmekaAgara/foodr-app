d731db12cd02f08b12912e8330f8e87f3963aee6
Emeka Agara <emekaagara@gmail.com>
2022-11-28 11:51:39 +0100

    home screen search bar done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {MapPin,UserIcon,ChevronDownIcon,SearchIcon,AdjustmentsIcon, MapPinIcon, ChatBubbleBottomCenterIcon, AdjustmentsVerticalIcon} from "react-native-heroicons/outline"
+import {MapPin,UserIcon,ChevronDownIcon,AdjustmentsIcon, MapPinIcon, ChatBubbleBottomCenterIcon, AdjustmentsVerticalIcon, BellAlertIcon, CheckBadgeIcon, MagnifyingGlassIcon} from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -17,7 +17,7 @@ const HomeScreen = () => {
     <SafeAreaView className="bg-white p-10 ">
       <View className='flex-row pb-3 items-center mx-4 space-x-2'>
       <View>
-        <AdjustmentsVerticalIcon color={"red"} size={30}/>
+        <CheckBadgeIcon color={"green"} size={25}/>
       </View>
 
         <View className='items-center flex-1'>
@@ -29,6 +29,16 @@ const HomeScreen = () => {
         <Image source={{uri:"https://emekaagara.com/wp-content/uploads/2021/06/emeka.jpg",}}
         className="h-8 w-8 bg-gray-200 p-4 rounded-full"
         />
+      </View>
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 rounded-md bg-gray-100 p-3.5">
+          <MagnifyingGlassIcon color="#9ca3af" size={20}/>
+          <TextInput
+            placeholder="Search resturants and meals"
+            keyboardType="default"
+          />
+        </View>
       </View>
     </SafeAreaView>
   )
