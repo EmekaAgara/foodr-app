7ef9ba90f9c180909630ede34a5c2d57067d5792
Emeka Agara <emekaagara@gmail.com>
2022-11-30 21:10:49 +0100

    restaurant dishes fixed

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,9 @@ import { Image, View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity';
-import { ArrowLeftIcon } from 'react-native-heroicons/outline';
+import { ArrowLeftIcon,BanknotesIcon, MapPinIcon } from 'react-native-heroicons/outline';
+import { StarIcon } from 'react-native-heroicons/solid';
+import DishRow from '../components/DishRow';
 
 const RestaurantScreen = () => {
     const navigation = useNavigation();
@@ -34,6 +36,46 @@ const RestaurantScreen = () => {
             <ArrowLeftIcon size={20}/>
         </TouchableOpacity>    
       </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+                <View className="flex-row items-center space-x-1">
+                    <StarIcon color="gold" size={22}/>
+                    <Text className="text-xs text-gray-500">{rating} • {genre}</Text>
+                    <MapPinIcon color="red" size={22}/>
+                    <Text className="text-xs text-gray-500">20 mins away • {address}</Text>
+                </View>
+            </View>
+            
+            <View className="flex-row space-x-2 mb-2">
+                <View className="flex-row items-center space-x-1">
+                <BanknotesIcon color="black" size={20}/>
+                <Text className="text-xs text-gray-500">N300 • Delivery Fee</Text>
+                </View>
+            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+        </View>   
+      </View>
+      <View>
+      <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+      
+      {dishes.map((dish) => (
+        <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+        />
+        ))}
+        </View>
+      
+      {/* <View className="flex-row flex-wrap -mx-2"> */}
+
+      {/* </View> */}
     </ScrollView>
   )
 }
