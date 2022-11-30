d0f1ed82a367eabc0f042da77f783bc886da9560
Emeka Agara <emekaagara@gmail.com>
2022-11-30 18:52:04 +0100

    restaurant screen image

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { urlFor } from '../sanity';
+import { ArrowLeftIcon } from 'react-native-heroicons/outline';
 
 const RestaurantScreen = () => {
+    const navigation = useNavigation();
+    const{
+        params:{id,fee,imgUrl,title,rating,genre,address,short_description,dishes,long,lat},
+    
+    } = useRoute();
+
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown:false,
+      });
+
+    }, [])
+
+
   return (
-    <View>
-      <Text>RestaurantScreen</Text>
-    </View>
+    <ScrollView>
+      <View className="relative">
+        <Image
+            source={{
+                uri:urlFor(imgUrl).url(),
+            }}
+            className="w-full h-96 bg-gray-200 p-4"
+        />
+        <TouchableOpacity
+            onPress={navigation.goBack}
+            className="absolute top-14 left-5 p-4 bg-gray-100 rounded-md">
+            <ArrowLeftIcon size={20}/>
+        </TouchableOpacity>    
+      </View>
+    </ScrollView>
   )
 }
 
